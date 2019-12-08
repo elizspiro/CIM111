9b516cc032c9a59fd490e19124c165836755e969
Elizabeth <exs915@miami.edu>
2019-12-07 19:33:34 -0500

    changes

---
+++ hw/final/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  /* all of your jacascript/jQuery code goes below */
+  /* css function .css("css property","value") */
+
+  $("#button1").click(function() {
+	$(".mapDescription").hide();
+	$("#desc1").show();
+  $("#descStart").hide();
+  });
+
+$("#button2").click(function() {
+	$(".mapDescription").hide();
+	$("#desc2").show();
+  $("#descStart").hide();
+  });
+
+$("#button3").click(function() {
+	$(".mapDescription").hide();
+	$("#desc3").show();
+  $("#descStart").hide();
+  });
+});
